95d3ecebea2340e43ddb68123075191357f3099c
Wilton Nicolas <nicolas.wilton51@gmail.com>
2023-05-13 21:02:09 -0300

    novo método - updateItem

--- src/Estoque.java
+++ src/Estoque.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Estoque {
 
     private String nomeDoEstoque;
     private int quantidadeDeItens = 0;
-    private ArrayList<Item> listaDeItens = new ArrayList<Item>();
+    private ArrayList<Item> listaDeItens = new ArrayList<>();
 
     public void setNomeDoEstoque(String nomeDoEstoque) {
         this.nomeDoEstoque = nomeDoEstoque;
@@ -27,6 +29,54 @@ public class Estoque {
 
     public ArrayList<Item> getListaDeItens(){
         return listaDeItens;
+    }
+
+    public void updateItem(){
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Digite o número do item que você deseja alterar: ");
+        int itemAlterar = teclado.nextInt();
+
+        while(itemAlterar > this.listaDeItens.size() || itemAlterar < 0){
+            System.out.print("Por favor digite um item válido a ser alterado: ");
+            itemAlterar = teclado.nextInt();
+        }
+
+        teclado.nextLine();
+
+        System.out.print("Digite quais atributos você deseja alterar (identificador e/ou quantidade): ");
+        String atributosAlterar = teclado.nextLine();
+
+        while(atributosAlterar.isEmpty() || !(atributosAlterar.toLowerCase().contains("identificador")) && !(atributosAlterar.toLowerCase().contains("quantidade"))){
+            System.out.print("Por favor digite pelo menos um atributo válido a ser alterado: ");
+            atributosAlterar = teclado.nextLine();
+        }
+
+        Item itemAtualizado = new Item();
+
+        if(atributosAlterar.toLowerCase().contains("identificador")){
+            System.out.print("Digite o novo identificador: ");
+            String novoIdentificador = teclado.nextLine();
+            itemAtualizado.setIdentificador(novoIdentificador);
+            System.out.print("O identificador do item foi alterado com sucesso.\n\n");
+        }
+        else{
+            itemAtualizado.setIdentificador(this.listaDeItens.get(itemAlterar).getIdentificador());
+        }
+
+        if(atributosAlterar.toLowerCase().contains("quantidade")){
+            System.out.print("Digite a nova quantidade de itens: ");
+            int novaQuantidade = teclado.nextInt();
+            itemAtualizado.setQuantidade(novaQuantidade);
+            System.out.print("A quantidade de itens foi alterada com sucesso.\n\n");
+        }
+        else{
+            itemAtualizado.setQuantidade(this.listaDeItens.get(itemAlterar).getQuantidade());
+        }
+
+        this.listaDeItens.set(itemAlterar, itemAtualizado);
+
     }
 
 }
